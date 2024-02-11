bc54567c020678f1df2aa419ff24dc5097441f2e
razayn <razayn@localhost.localdomain>
2024-02-11 08:05:06 +0530

    improved mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@
 import React, { useState, useEffect } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import styles from '../../styles/Navbar.module.css';
-
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [isSearchActive, setIsSearchActive] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); 
 
   useEffect(() => {
     const handleKeyPress = (e) => {
@@ -32,17 +32,9 @@ const Navbar = () => {
     setIsSearchActive(false);
   };
 
-  function smoothScrollTo(event, targetId) {
-    event.preventDefault();
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth' // Smooth scrolling behavior
-      });
-    }
-  }
-  
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <nav className={`${styles.navbar} ${isSearchActive ? styles['search-active'] : ''}`}>
@@ -50,20 +42,28 @@ const Navbar = () => {
         <div className={styles['navbar-left']}>
           <span className={styles['navbar-heading']}>rasayan_</span>
         </div>
-        
+
         <div className={styles['navbar-right']}>
           <div className={styles['navbar-center']}>
-            {/* Smooth scroll to the about section */}
-            <Link href="#about" scroll={true}>About</Link>
-            <Link href="#work" scroll={true}>Work</Link>
-            <Link href="#connect" scroll={true}>Connect</Link>
+            {/* Mobile menu toggle button */}
+            <button className={styles['mobile-menu-toggle-button']} onClick={toggleMobileMenu}>
+              Menu
+            </button>
+            {/* Mobile menu items */}
+            {isMobileMenuOpen && (
+              <div className={styles['mobile-menu']}>
+                <Link href="#about" scroll={true}>About</Link>
+                <Link href="#work" scroll={true}>Work</Link>
+                <Link href="#connect" scroll={true}>Connect</Link>
+              </div>
+            )}
           </div>
           <a href='https://github.com/Rasayan/prt'><FaGithub className={styles['github-icon']} /></a>
         </div>
       </div>
     </nav>
   );
-  
 };
 
 export default Navbar;
+
